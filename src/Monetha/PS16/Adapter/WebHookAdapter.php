b15a3125e6e523631e6455e113c98b43e5aa47bd
Alexander Hitrov <alexander.hitrov@monetha.io>
2019-04-02 17:25:49 +0300

    require monetha/payment-plugin-php-sdk": "dev-master

--- src/Monetha/PS16/Adapter/WebHookAdapter.php
+++ src/Monetha/PS16/Adapter/WebHookAdapter.php
@@ -2,11 +2,10 @@
 
 namespace Monetha\PS16\Adapter;
 
-use Monetha\Constants\EventType;
-use Monetha\Constants\Resource;
 use Order;
+use Monetha\Adapter\WebHookAdapterAbstract;
 
-class WebHookAdapter {
+class WebHookAdapter extends WebHookAdapterAbstract {
     /**
      * @var Order
      */
@@ -17,55 +16,28 @@ class WebHookAdapter {
         $this->order = $order;
     }
 
-    public function processWebHook($data)
-    {
-        switch ($data->resource) {
-            case Resource::ORDER:
-                switch ($data->event) {
-                    case EventType::CANCELLED:
-                        return $this->cancelOrder($data->payload->note);
-
-                    case EventType::FINALIZED:
-                        return $this->finalizeOrder();
-
-                    case EventType::MONEY_AUTHORIZED:
-                        return $this->finalizeOrderByCard();
-
-                    default:
-                        throw new \Exception('Bad action type');
-                }
-
-            default:
-                throw new \Exception('Bad resource');
-        }
-    }
-
-    private function cancelOrder($note)
-    {
+    public function cancel($note) {
         $history = new \OrderHistory();
         $history->id_order = $this->getOrderId();
         $history->changeIdOrderState(6, $this->getOrderId(), true);
         return $history->save();
     }
 
-    private function finalizeOrder()
-    {
+    public function finalize() {
         $history = new \OrderHistory();
         $history->id_order = $this->getOrderId();
         $history->changeIdOrderState(2, $this->getOrderId(), true);
         return $history->save();
     }
 
-    private function finalizeOrderByCard()
-    {
+    public function authorize() {
         $history = new \OrderHistory();
         $history->id_order = $this->getOrderId();
         $history->changeIdOrderState(2, $this->getOrderId(), true);
         return $history->save();
     }
 
-    private function getOrderId()
-    {
+    public function getOrderId() {
         return (int) $this->order->id;
     }
 }
